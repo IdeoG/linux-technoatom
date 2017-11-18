ad7568c64ae7ea21e88fc1885d8b4a0551e8e7d0
ideogaib <ideogaib@gmail.com>
2017-11-18 15:01:28 +0300

    02: fork() example

---
+++ 02/fork.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+int main()
+{
+    int id;
+    id = fork();
+    printf("id value : %d\n", id);
+    if (id == 0)
+    {
+        printf("Child : Hello I am the child process\n");
+        printf("Child : Child’s PID: %d\n", getpid());
+        printf("Child : Parent’s PID: %d\n", getppid());
+    }
+    else
+    {
+        printf("Parent : Hello I am the parent process\n");
+        printf("Parent : Parent’s PID: %d\n", getpid());
+        printf("Parent : Child’s PID: %d\n", id);
+    }
+
+    return 0;
+}
