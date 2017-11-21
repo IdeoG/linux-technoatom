22b32991673f30048955f2173782864beca41093
ideog <ideogaib@gmail.com>
2017-11-21 16:56:17 +0300

    some tests with forks

--- 02/fork.c
+++ 02/fork.c
@@ -4,26 +4,30 @@
  * getpid and getppid return two different values
  * https://stackoverflow.com/questions/15183427/getpid-and-getppid-return-two-different-values
  */
-int main()
-{
-    int id;
-    id = fork();
-    printf("fork id value : %d\n", id);
+void is_parent(int id) {
     if (id == 0)
     {
         printf("Child : Hello I am the child process\n");
         printf("Child : Child’s PID: %d\n", getpid());
-        printf("Child : Parent’s PID: %d\n", getppid());
+        printf("Child : Parent’s PID: %d\n\n", getppid());
     }
     else
     {
         printf("Parent : Hello I am the parent process\n");
         printf("Parent : Parent’s PID: %d\n", getpid());
-        printf("Parent : Child’s PID: %d\n", id);
+        printf("Parent : Child’s PID: %d\n\n", id);
     }
+}
 
+int main() {
+    int id;
     id = fork();
-    printf("fork id value before close : %d\n", id);
-
+    printf("fork id value : %d\n", id);
+    is_parent(id);
+    
+    id = fork();
+    printf("fork id value : %d\n", id);
+    is_parent(id);
+    
     return 0;
 }
--- 02/system.c
+++ 02/system.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main()
-{
+
+int main() {
     system("echo \"Test\" > log.txt");
     return 0;
 }
